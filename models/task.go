5b7fac3cfe05800d8eaccb913e533ed9294b9776
jamfly <cocoredju222@gmail.com>
2019-06-06 12:29:35 +0800

    Add priority

--- models/task.go
+++ models/task.go
@@ -8,4 +8,5 @@ type Task struct {
 	ID          bson.ObjectId `bson:"_id" json:"id"`
 	Name        string        `bson:"name" json:"name"`
 	Description string        `bson:"description" json:"description"`
+	Priority    string        `bson:"priority" json:"priority"`
 }
